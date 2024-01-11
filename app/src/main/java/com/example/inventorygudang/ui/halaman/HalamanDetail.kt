9b2c1730f3c925a13b58d48b95baf54782217d34
MuhFadila <muh.fadila18@gmail.com>
2024-01-11 10:42:30 +0700

    create fun DeleteConfirmationDialog at HalamanDetail by fadil

--- app/src/main/java/com/example/inventorygudang/ui/halaman/HalamanDetail.kt
+++ app/src/main/java/com/example/inventorygudang/ui/halaman/HalamanDetail.kt
@@ -1,5 +1,11 @@
 package com.example.inventorygudang.ui.halaman
 
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import com.example.inventorygudang.R
 import com.example.inventorygudang.navigasi.DestinasiNavigasi
 
@@ -8,4 +14,24 @@ object DetailsDestination : DestinasiNavigasi {
     override val titleRes = R.string.detail_barang
     const val barangIdArg = "itemId"
     val routeWithArgs = "$route/{$barangIdArg}"
+}
+
+@Composable
+private fun DeleteConfirmationDialog(
+    onDeleteConfirm: () -> Unit, onDeleteCancel: () -> Unit, modifier: Modifier = Modifier
+) {
+    AlertDialog(onDismissRequest = { /* Do nothing */ },
+        title = { Text(stringResource(R.string.attention)) },
+        text = { Text(stringResource(R.string.delete)) },
+        modifier = modifier,
+        dismissButton = {
+            TextButton(onClick = onDeleteCancel) {
+                Text(text = stringResource(R.string.no))
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onDeleteConfirm) {
+                Text(text = stringResource(R.string.yes))
+            }
+        })
 }
