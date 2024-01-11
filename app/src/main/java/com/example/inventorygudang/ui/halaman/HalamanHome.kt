1025c8dfc99b65c74fe8be40fa3ad73aa32a9ada
MuhFadila <muh.fadila18@gmail.com>
2024-01-11 10:02:43 +0700

    create function ListBarang at HalamanHome by alwan

--- app/src/main/java/com/example/inventorygudang/ui/halaman/HalamanHome.kt
+++ app/src/main/java/com/example/inventorygudang/ui/halaman/HalamanHome.kt
@@ -1,11 +1,14 @@
 package com.example.inventorygudang.ui.halaman
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.Card
@@ -20,6 +23,23 @@ import androidx.compose.ui.unit.dp
 import com.example.inventorygudang.R
 import com.example.inventorygudang.data.Barang
 
+@Composable
+fun ListBarang(
+    itemBarang: List<Barang>,
+    modifier: Modifier = Modifier,
+    onItemClick: (Barang) -> Unit
+) {
+    LazyColumn(modifier = Modifier) {
+        items(items = itemBarang, key = { it.id }) { person ->
+            DataBarang(
+                barang = person,
+                modifier = Modifier
+                    .padding(dimensionResource(id = R.dimen.padding_small))
+                    .clickable { onItemClick(person) }
+            )
+        }
+    }
+}
 @Composable
 fun DataBarang(
     barang: Barang,
