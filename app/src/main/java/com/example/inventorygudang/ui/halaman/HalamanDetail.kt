0daccf31d7c99b709cafd3b0bf12c9de1824d58b
MuhFadila <muh.fadila18@gmail.com>
2024-01-11 10:43:01 +0700

    create fun ItemDetailsRow at HalamanDetail by alwan

--- app/src/main/java/com/example/inventorygudang/ui/halaman/HalamanDetail.kt
+++ app/src/main/java/com/example/inventorygudang/ui/halaman/HalamanDetail.kt
@@ -1,11 +1,15 @@
 package com.example.inventorygudang.ui.halaman
 
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import com.example.inventorygudang.R
 import com.example.inventorygudang.navigasi.DestinasiNavigasi
 
@@ -14,6 +18,17 @@ object DetailsDestination : DestinasiNavigasi {
     override val titleRes = R.string.detail_barang
     const val barangIdArg = "itemId"
     val routeWithArgs = "$route/{$barangIdArg}"
+}
+
+@Composable
+private fun ItemDetailsRow(
+    @StringRes labelResID: Int, itemDetail: String, modifier: Modifier = Modifier
+) {
+    Row(modifier = modifier) {
+        Text(text = stringResource(labelResID))
+        Spacer(modifier = Modifier.weight(1f))
+        Text(text = itemDetail, fontWeight = FontWeight.Bold)
+    }
 }
 
 @Composable
