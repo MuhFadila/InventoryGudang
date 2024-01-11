ea714d02a6c4bb80590caa8479b7a83a260075d6
MuhFadila <muh.fadila18@gmail.com>
2024-01-11 11:02:04 +0700

    done by together

--- app/src/main/java/com/example/inventorygudang/MainActivity.kt
+++ app/src/main/java/com/example/inventorygudang/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.inventorygudang
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,9 +12,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.inventorygudang.navigasi.BarangApp
 import com.example.inventorygudang.ui.theme.InventoryGudangTheme
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,7 +26,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    BarangApp()
                 }
             }
         }
