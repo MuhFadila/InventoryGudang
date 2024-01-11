c557e1075c304b30ca901a4c5c78382fe8e43a66
MuhFadila <muh.fadila18@gmail.com>
2024-01-11 10:26:53 +0700

    add fun EntryBarangBody at HalamanEntry by alwan

--- app/src/main/java/com/example/inventorygudang/ui/halaman/HalamanEntry.kt
+++ app/src/main/java/com/example/inventorygudang/ui/halaman/HalamanEntry.kt
@@ -11,8 +11,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -33,7 +35,37 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.example.inventorygudang.R
 import com.example.inventorygudang.model.DetailBarang
+import com.example.inventorygudang.model.UIStateBarang
 import java.util.Calendar
+
+
+@RequiresApi(Build.VERSION_CODES.O)
+@Composable
+fun EntryBarangBody(
+    uiStateBarang: UIStateBarang,
+    onBarangValueChange: (DetailBarang) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_large)),
+        modifier = modifier.padding(dimensionResource(id = R.dimen.padding_medium))
+    ) {
+        FormInputBarang(
+            detailBarang = uiStateBarang.detailBarang,
+            onValueChange = onBarangValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            enabled = uiStateBarang.isEntryValid,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(stringResource(R.string.btn_submit))
+        }
+    }
+}
 
 
 
